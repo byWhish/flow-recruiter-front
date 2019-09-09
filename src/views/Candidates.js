623c9b6097b600a4d7cb0e96e9c5d0f7690aceb3
Gerardo Caceres <gpcaceres@teco.com.ar>
2019-09-09 06:38:14 -0300

    kickstart home and candidates view

---
+++ src/views/Candidates.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Candidates.module.css';
+
+const Candidates = () => (
+    <div className={styles.candidates}>
+        Mails
+    </div>
+);
+
+export default Candidates;
---
+++ src/views/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => (
+    <div className={styles.home}>
+        Home
+    </div>
+);
+
+export default Home;
